31e5b5740c1c001f2c43e14c615f4c95ee5e0fbe
Nicki <muecelee@web.de>
2026-05-23 10:02:08 +0200

    Backend: Add models Color & ColorUpdate

---
+++ backend/models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Color(BaseModel):
+    id: int
+    article_number: str
+    shelf_number: Optional[str] = None
+    name: str
+    hex: Optional[str] = None
+    description: Optional[str] = None
+    brand: str
+    owned: bool
+    comment: Optional[str] = None
+
+
+class ColorUpdate(BaseModel):
+    owned: Optional[bool] = None
+    comment: Optional[str] = None
