a3b8debf2e57cc1138b49568eff29590c2daa6d2
schaenick <muecelee@web.de>
2026-06-09 14:30:31 +0200

    Added first frontend components

--- frontend/bottlecap/App.tsx
+++ frontend/bottlecap/App.tsx
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Color } from "./types/color";
+import { getColors } from './services/api';
 
-export default function App() {
+
+export default function App(){
+  const [colors, setColors] = useState<Color[]>([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+      const load = async () => {
+          const data = await getColors()
+          setColors(data)
+          console.log(colors)
+          setLoading(false)  
+      }
+      load(); 
+  }, []) 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <Text>Hallo</Text>
     </View>
-  );
+)
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
